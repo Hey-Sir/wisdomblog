3b9199fb2b42c520a3d24864f687cacbacd3f789
cai <852858682@qq.com>
2018-08-14 14:55:48 +0800

    修复标签空间

--- src/main/resources/static/js/userspace/blogedit.js
+++ src/main/resources/static/js/userspace/blogedit.js
@@ -20,18 +20,6 @@ $(function() {
             return this._bkGetActionUrl.call(this, action);
         }
     }
-
-	// 初始化 md 编辑器
-    $("#md").markdown({
-        language: 'zh',
-        fullscreen: {
-            enable: true
-        },
-        resize:'vertical',
-        localStorage:'md',
-        imgurl: 'http://localhost:8081',
-        base64url: 'http://localhost:8081'
-    });
   
     // 初始化标签控件
     $('.form-control-tag').tagEditor({
@@ -44,28 +32,10 @@ $(function() {
     });
     
     $('.form-control-chosen').chosen();
- 
- 	$("#uploadImage").click(function() {
-		$.ajax({
-		    url: 'http://localhost:8081/upload',
-		    type: 'POST',
-		    cache: false,
-		    data: new FormData($('#uploadformid')[0]),
-		    processData: false,
-		    contentType: false,
-		    success: function(data){
-		    	var mdcontent=$("#md").val();
-		    	 $("#md").val(mdcontent + "\n![]("+data +") \n");
- 
-	         }
-		}).done(function(res) {
-			$('#file').val('');
-		}).fail(function(res) {});
- 	})
- 
+
  	// 发布博客
  	$("#submitBlog").click(function() {
- 
+
 		// 获取 CSRF Token 
 		var csrfToken = $("meta[name='_csrf']").attr("content");
 		var csrfHeader = $("meta[name='_csrf_header']").attr("content");
